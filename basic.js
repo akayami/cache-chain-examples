129268634a1ef33b9d3008eb2d5e64eb0fb7c9c4
Tomasz Rakowski <tomasz.rakowski@jomediainc.com>
2015-11-30 17:04:53 -0500

    Basic example

---
+++ basic.js
@@ -0,0 +1,34 @@
+var redis = require('redis');
+var cc = require('cache-chain');
+var ccr = require('cache-chain-redis');
+var ccm = require('cache-chain-memory');
+
+var chain = cc.chain({
+	ttl: 10000,			// Setting default chain timeouts
+	stale: 10000 * 2
+});
+
+var redisClient = redis.createClient();
+var layerRedis = cc.layer(ccr(redisClient));
+var layerMemory = cc.layer(ccm());
+layerMemory.append(layerRedis);
+chain.append(layerMemory);
+
+var key = "key";
+var value = "value";
+
+chain.set(key, value, {ttl: 10, stale: 5}, function(err, reply) {
+	if (err) {
+		console.error('Error occured');
+		console.error(err);
+	} else {
+		chain.get(key, {ttl: 10, stale: 5}, function(err, reply) {
+			if(value === reply) {
+				console.log('OK');
+			} else {
+				console.log('Something happened');
+			}
+			redisClient.end();
+		});
+	}
+})
